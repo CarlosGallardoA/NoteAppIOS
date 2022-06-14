956f674cbe89214900cc36411d589b584bccb95d
Carlos <carlitos.gallardo08@hotmail.com>
2022-06-14 12:36:25 -0500

    Initial Commit

---
+++ noteapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  noteapp
+//
+//  Created by Carlos Enrique Gallardo Ambrosio on 14/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ noteapp/noteappApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteappApp.swift
+//  noteapp
+//
+//  Created by Carlos Enrique Gallardo Ambrosio on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct noteappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
